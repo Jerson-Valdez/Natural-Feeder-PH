120375e3c1867d58c7d2e542bbc5fa099128ebc8
Jerson <valdezjerson8624@gmail.com>
2026-08-19 17:11:52 +0800

    refactoring where the icon located

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/logo.svg";
+import { logo } from "../../assets/Logo.svg";
 import { IconShoppingBag } from "@tabler/icons-react";
 
 export default function Header({ basketCount = 0 }) {
